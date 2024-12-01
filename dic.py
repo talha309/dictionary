9de5dc94bd85ed5ba5f164133d9aa6aac01be9db
talha309 <tiseoexpert@gmail.com>
2024-12-02 00:19:34 +0500

    write  new code

--- dic.py
+++ dic.py
@@ -31,3 +31,30 @@ else:
     print ("false")
 # Count the total number of keys in the student dictionary.
 print (len(student))
+# Advanced Dictionary Usage
+# Merge the following two dictionaries and print the result:
+dict1 = {'a': 1, 'b': 2}  
+dict2 = {'c': 3, 'd': 4}  
+dict1.update(dict2)
+print ( dict1)
+
+# Create a dictionary from a list of tuples: [('name', 'Alice'), ('age', 25), ('city', 'Paris')].
+dictionary_list = [{'name':'alice'},{'age':'25'},{'city':'Paris'}]
+print (dictionary_list)
+
+# Sort the keys of the dictionary {'z': 1, 'a': 2, 'c': 3} in ascending order and print the sorted dictionary.
+dictionary = {'z': "1", 'a': "2", 'c': '3'}
+sorted_dictionary={keys:dictionary[keys] for keys in sorted(dictionary)}
+print (sorted_dictionary)
+
+# Reverse the dictionary {'a': 1, 'b': 2, 'c': 3} so that keys become values and values become keys.
+dictionary1 = {'a': '1', 'b': '2', 'c': "3"}
+reversed_dict= {values : keys for keys , values in dictionary1.items()}
+print (reversed_dict)
+# Write a Python function to check if two dictionaries are identical (contain the same key-value pairs).
+def dictionary_function(dic3,dic4):
+    dict5 = dic3 is dic4 
+    return dict5
+dict1={'a':'34','b':'45'}
+dict2={'a':'34','b':'54'}
+print  (dictionary_function(dict1,dict2))
